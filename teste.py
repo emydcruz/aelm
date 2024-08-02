ab28c2405a464fdd343f83e4b0f19eca433fa400
Luiza Vanhoni <160127195+luttwy@users.noreply.github.com>
2024-08-02 10:01:00 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,13 @@
+#Isso serve para mostrar olá mundo
+
+
+"""
+Este é um comentário 
+e mais um
+e outro """
+
+print("Seja bem vindo ai")
+
+nome = input ("Qual seu nome?")
+
+print ("bem vindo","nome")
