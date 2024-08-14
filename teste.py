6d5556335ef32f14e799a54bc7ea9e3adef63657
Amanda Martins <163011471+mandinhaaa007@users.noreply.github.com>
2024-08-14 09:57:34 -0300

    Update teste.py

---
+++ teste.py
@@ -0,0 +1,31 @@
+#Isso serve para mostrar olá mundo
+
+
+"""
+Este é um comentário 
+e mais um
+e outro """
+
+print("Seja bem vindo ai")
+
+nome = input ("Qual seu nome?")
+
+print("bem vindo","nome")
+print("Posso ajudar?")
+print("1-gatinho preto")
+print("2-gatinho laranja")
+print("3-gatinho mesclado")
+
+opcao = int(input("Escolha um gatinho"))
+
+if opcao == 1:
+    print("gatinho preto")
+
+elif opcao == 2:
+    print("gatinho laranja")
+
+elif opcao == 3:
+    print("gatinho mesclado")
+
+    else:
+        print("esse gatinho fugiu, escolhe outro.")
