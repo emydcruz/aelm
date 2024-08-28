85b06e0867cb25835ddd8d7bcff2934687a6cea7
Amanda Martins <163011471+mandinhaaa007@users.noreply.github.com>
2024-08-28 08:59:56 -0300

    Finalizando O programa

--- teste.py
+++ teste.py
@@ -5,30 +5,34 @@ import time
 # o código abaixo mostra uma saudação
 
 
-  print("Seja bem vindo")
+tempo = 2
 
-nome = input("Qual seu nome?")
+def saudacao():
+    print("Seja bem vindo")
 
-   print("bem vindo",nome)
-time.sleep(5)
+    nome = input("Qual seu nome?")
+
+    print("bem vindo",nome)
+    time.sleep(tempo)
 
 def menu():
-   print("Como posso ajudar?")
-   print("1-impala")
-   print("2-risqué")
-   print("3-colorama")
+    print("Como posso ajudar?")
+    time.sleep(tempo)
+    print("1-impala")
+    print("2-risqué")
+    print("3-colorama")
 
-opcao = int(input("escolha a opção desejada"))
+    opcao = int(input("escolha a opção desejada"))
 
-if opcao == 1:
-   print("impala:praíso, serena, gatinha")
-elif opcao == 2:
-   print("risqué:lágrimas de vênus, rebu, carmin")
-elif opcao == 3:
-   print("colorama: 40 graus, branco lunar, nude")
+    if opcao == 1:
+        print("impala:praíso, serena, gatinha")
+    elif opcao == 2:
+        print("risqué:lágrimas de vênus, rebu, carmin")
+    elif opcao == 3:
+        print("colorama: 40 graus, branco lunar, nude")
 
-else:
-   print("não há está opção")
+    else:
+        print("não há está opção")
 
-
+saudacao()
 menu()
